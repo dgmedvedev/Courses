e20eb06726c79d28deb881d8614c06ab7f4b7d23
dgmedvedev <dg-medvedev@mail.ru>
2025-04-29 18:41:03 +0300

    The UI of the OnboardingFragment is completed

--- app/src/main/java/com/medvedev/courses/presentation/MainActivity.kt
+++ app/src/main/java/com/medvedev/courses/presentation/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.medvedev.courses.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,7 +14,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.activityMain) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
