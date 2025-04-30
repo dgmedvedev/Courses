ed92d63efd11c9d48bb571e4693d840a1bd914c3
dgmedvedev <dg-medvedev@mail.ru>
2025-04-30 20:37:10 +0300

    The UI of the AccountFragment is completed

--- app/src/main/java/com/medvedev/courses/presentation/MainActivity.kt
+++ app/src/main/java/com/medvedev/courses/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.medvedev.courses.R
 import com.medvedev.courses.databinding.ActivityMainBinding
 import dev.androidbroadcast.vbpd.viewBinding
@@ -20,5 +21,24 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.bottomNavigation.setOnNavItemClickListener { index ->
+            when (index) {
+                0 -> launchFragment(MainFragment.getInstance())
+                1 -> launchFragment(FavoritesFragment.getInstance())
+                2 -> launchFragment(AccountFragment.getInstance())
+                else -> return@setOnNavItemClickListener
+            }
+        }
+    }
+
+    private fun launchFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_main, fragment)
+            .commit()
     }
 }
